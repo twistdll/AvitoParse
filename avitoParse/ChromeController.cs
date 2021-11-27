392964564b835fb6fe51af0e806118d27051cfd7
twistballz <f.lala02@mail.ru>
2021-11-27 21:56:22 +0300

    Набросал основной функционал и кривоватую архитектуру.

---
+++ avitoParse/ChromeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace avitoParse
+{
+    class ChromeController
+    {
+        private ChromeDriver _chromeDriver;
+        private ChromeOptions _chromeOptions;
+
+        public ChromeController()
+        { 
+            _chromeOptions = new ChromeOptions();
+            _chromeOptions.AddArgument("headless");
+            _chromeOptions.AddArgument("start-maximized");
+        }
+
+        public void StartParsing()
+        {
+            _chromeDriver = new ChromeDriver(_chromeOptions);
+            _chromeDriver.Navigate().GoToUrl("https://www.avito.ru/" + "smolensk");
+            EnterSearchQuery("мопс");
+            Thread.Sleep(2000);
+            GetAdsList();
+        }
+
+        private void EnterSearchQuery(string queryText)
+        {
+            IWebElement searchBox = _chromeDriver.FindElement(By.CssSelector(".input-layout-input-layout-_HVr_"));
+            searchBox.Click();
+            searchBox.SendKeys(queryText + Keys.Enter);        
+        }
+
+        private void GetAdsList()
+        {
+            var ads = _chromeDriver.FindElements(By.CssSelector(".iva-item-root-Nj_hb"));
+        }
+    }
+}
--- avitoParse/Form1.cs
+++ avitoParse/Form1.cs
@@ -20,6 +20,20 @@ namespace avitoParse
         private void Form1_Load(object sender, EventArgs e)
         {
             siticoneShadowForm1.SetShadowForm(this);
+            ChromeController driver = new ChromeController();
+            driver.StartParsing();
+        }
+    }
+
+    struct SearchInfo
+    {
+        private string _regionName;
+        private string _queryText;
+
+        public SearchInfo(string regionName, string queryText)
+        {
+            _regionName = regionName;
+            _queryText = queryText;
         }
     }
 }
